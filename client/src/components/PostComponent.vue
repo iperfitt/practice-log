e1dae168d334a5f4a743e79466ab816895c79c0a
allisonkydy <allisnelly@gmail.com>
2020-03-31 22:55:33 -0500

    implemented delete and create post in PostComponent.vue

--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
     <h1>Latest Posts</h1>
-    <!-- CREATE POST HERE -->
+    <div class="create-post">
+      <label for="create-post">Say Something...</label>
+      <input type="text" id="create-post" v-model="text" placeholder="Create a post">
+      <button v-on:click="createPost">Post!</button>
+    </div>
     <hr>
     <p class="error" v-if="error"> {{error}} </p>
     <div class="posts-container">
@@ -10,6 +14,7 @@
         v-bind:item="post"
         v-bind:index="index"
         v-bind:key="post._id"
+        v-on:dblclick="deletePost(post._id)"
       >
         {{ `${post.createdAt.getDate()} / ${post.createdAt.getMonth()} /
         ${post.createdAt.getFullYear()}` }}
@@ -36,6 +41,16 @@ export default {
       this.posts = await PostService.getPosts();
     } catch (err) {
       this.error = err.message;
+    }
+  },
+  methods: {
+    async createPost() {
+      await PostService.insertPost(this.text)
+      this.posts = await PostService.getPosts();
+    },
+    async deletePost(id) {
+      await PostService.deletePost(id)
+      this.posts = await PostService.getPosts();
     }
   }
 }
